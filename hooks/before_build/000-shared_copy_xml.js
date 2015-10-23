401e7f29d279821399f14d74410200fc3131a1d7
JrontEnd <mingxiruliu@gmail.com>
2015-10-23 09:32:31 +0800

    Update 000-shared_copy_xml.js

--- hooks/before_build/000-shared_copy_xml.js
+++ hooks/before_build/000-shared_copy_xml.js
@@ -1,13 +1,19 @@
 module.exports = function(context) {
 
   /** @external */
-      var fs = context.requireCordovaModule('fs'),
-          path = context.requireCordovaModule('path'),
-          ConfigParser = context.requireCordovaModule("cordova-lib/src/configparser/ConfigParser"),
-          XmlHelpers = context.requireCordovaModule("cordova-lib/src/util/xml-helpers"),
-          et = context.requireCordovaModule('elementtree');
-          
-  var androidPlatformDir = path.join(context.opts.projectRoot,
-            'platforms', 'android');
-        
-    
+  var fs = context.requireCordovaModule('fs'),
+      path = context.requireCordovaModule('path'),
+      ConfigParser = context.requireCordovaModule("cordova-lib/src/configparser/ConfigParser"),
+      XmlHelpers = context.requireCordovaModule("cordova-lib/src/util/xml-helpers"),
+      et = context.requireCordovaModule('elementtree');
+       
+  /** @defaults */   
+  var pluginCopyFile = path.join(context.opts.plugin.dir, 'res','xwalk_app_strings.xml'), 
+      androidPlatformDir = path.join(context.opts.projectRoot,
+            'platforms', 'android'),
+      copyTarget = androidPlatformDir+'/res/values-zh-rCN/strings.xml';
+      
+  /** Init */
+  var projectResXml = XmlHelpers.parseElementtreeSync(pluginCopyFile);
+  fs.writeFileSync(copyTarget, projectResXml.write({indent: 4}), 'utf-8');
+};
