f7df863ebd418ac776ca3542c283383c4f471a64
JrontEnd <mingxiruliu@gmail.com>
2015-11-19 19:13:26 +0800

    Update 000-shared_copy_xml.js

--- hooks/before_build/000-shared_copy_xml.js
+++ hooks/before_build/000-shared_copy_xml.js
@@ -3,8 +3,8 @@ module.exports = function(context) {
   /** @external */
   var fs = context.requireCordovaModule('fs'),
       path = context.requireCordovaModule('path'),
-      ConfigParser = context.requireCordovaModule("cordova-lib/src/configparser/ConfigParser"),
-      XmlHelpers = context.requireCordovaModule("cordova-lib/src/util/xml-helpers"),
+      ConfigParser =require('cordova-common').configParser,
+      XmlHelpers = require('cordova-common').xmlHelpers,
       et = context.requireCordovaModule('elementtree');
        
   /** @defaults */   
